34be91c192a23c80945b87a35467a9dfd7e17d46
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2022-11-26 11:38:18 +0700

    7. create splashpage, down sdk, nullsafety disable

---
+++ lib/pages/sign_in_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SingInPage extends StatelessWidget {
+  const SingInPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Sing In Page',
+        ),
+      ),
+    );
+  }
+}
