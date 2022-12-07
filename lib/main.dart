9b9d58d1385ac05302908e5491f75acfdefdaa83
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2022-12-07 19:29:30 +0700

    19 detail chat page bagian header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamos/pages/detail_chat_page.dart';
 import 'package:shamos/pages/home/main_page.dart';
 import 'package:shamos/pages/sign_in_page.dart';
 import 'package:shamos/pages/sign_up_page.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         '/sign-in': (context) => const SignInPage(),
         '/sign-up': (context) => const SignUpPage(),
         '/home': (context) => const MainPage(),
+        '/detail-chat': (context) => const DetailChatPage(),
       },
     );
   }
---
+++ lib/pages/detail_chat_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:shamos/theme.dart';
+
+class DetailChatPage extends StatelessWidget {
+  const DetailChatPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          backgroundColor: backgroundColor1,
+          centerTitle: false,
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/icon_shop_logo_online.png',
+                width: 45,
+              ),
+              SizedBox(width: 12),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Shoe Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: medium,
+                    ),
+                  ),
+                  Text(
+                    'Online',
+                    style: secondTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: light,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+    );
+  }
+}
