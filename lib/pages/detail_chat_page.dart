8b3a91978065402b9a5306bec1f5c392463c15fe
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2022-12-10 16:56:48 +0700

    21 detail chat page bagian 2

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -8,7 +8,7 @@ class DetailChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget header() {
       return PreferredSize(
-        preferredSize: Size.fromHeight(60),
+        preferredSize: const Size.fromHeight(60),
         child: AppBar(
           backgroundColor: backgroundColor1,
           centerTitle: false,
@@ -18,7 +18,7 @@ class DetailChatPage extends StatelessWidget {
                 'assets/icon_shop_logo_online.png',
                 width: 45,
               ),
-              SizedBox(width: 12),
+              const SizedBox(width: 12),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -44,35 +44,95 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    Widget productPreview() {
+      return Container(
+        width: 225,
+        height: 74,
+        margin: EdgeInsets.only(bottom: 20),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColor4,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: primaryColor,
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              //widget agar gambar bisa memiliki sisi melengkung
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISION new era',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/button_close.png',
+              width: 20,
+            ),
+          ],
+        ),
+      );
+    }
+
     Widget inputChat() {
       return Container(
         margin: const EdgeInsets.all(20),
-        child: Row(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Container(
-                height: 45,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                decoration: BoxDecoration(
-                  color: backgroundColor4,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: TextFormField(
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'Type Message',
-                      hintStyle: subtitleTextStyle,
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: backgroundColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(width: 20),
-            Image.asset(
-              'assets/button_send.png',
-              width: 45,
+                SizedBox(width: 20),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 45,
+                ),
+              ],
             ),
           ],
         ),
