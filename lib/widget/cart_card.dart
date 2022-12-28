27cbf27cb484a8f846cbc4ec34c6ff7743934b26
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2022-12-28 16:45:26 +0700

    36 cart page bagian cart card

---
+++ lib/widget/cart_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+// import 'package:shamos/theme.dart';
+
+class CartCard extends StatelessWidget {
+  const CartCard({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
