efaaa150f2424ca8e947f768c7bb19ef02cc322d
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2023-01-04 11:06:27 +0700

    51 Menambahkan Model Product,Category, dan Gallery

---
+++ lib/models/category_model.dart
@@ -0,0 +1,19 @@
+class CategoryModel {
+  int id;
+  String name;
+
+  // contructure
+  CategoryModel({this.id, this.name});
+
+  CategoryModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+}
